8011e0aa0742381a43ca23501aa0300e6d59ea28
denysenko-dev <dmytro.denysenko.dev@gmail.com>
2026-07-18 20:58:35 +0300

    lesson-20-21 fix: use color-gray spelling consistently

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-document.querySelector('#my-name').innerHTML = `${data.first_name} <span class="color-grey">${data.last_name}</span>`;
+document.querySelector('#my-name').innerHTML = `${data.first_name} <span class="color-gray">${data.last_name}</span>`;
 document.querySelector('#job-title').textContent = data.job_title;
 document.querySelector('.profile-data').textContent = data.profile;
 /**
